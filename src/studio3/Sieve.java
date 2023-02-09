5f02b5eb2e3dcf792809934017e024beb5fc76dc
RealSeanHan <h.xuan1@wustl.edu>
2023-02-09 14:15:06 -0600

    Studio3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,19 @@
+package studio3;
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 40;
+		boolean[] A = new boolean[n-1];
+		for (int i = 2; i < A.length; i++)
+		{
+			A[i] = True;
+		    System.out.println(  A[i] );
+		}
+		
+		System.out.println(  "\n" +A[2] );
+
+	    }
+
+	}
+
